0728b6e2ba321ee354b023f5a21025758ac3fb9f
tsvirkodmitry1 <tsvirkodmitry1@gmail.com>
2021-12-14 21:32:05 +0300

    "edit config.yml for circleci"

--- src/test/java/tests/TestListener.java
+++ src/test/java/tests/TestListener.java
@@ -8,21 +8,19 @@ import org.testng.ITestResult;
 import utils.AllureUtils;
 
 
-
 public class TestListener implements ITestListener {
 
 
-    @Override
     public void onTestStart(ITestResult result) {
         System.out.println("Test started");
     }
 
-    @Override
+
     public void onTestSuccess(ITestResult result) {
         System.out.println(String.format("Test '%s' succeed", result.getName()));
     }
 
-    @Override
+
     public void onTestFailure(ITestResult result) {
         System.out.println(String.format("Test '%s' failed", result.getName()));
         WebDriver driver = (WebDriver) (result.getTestContext().getAttribute("driver"));
@@ -30,24 +28,22 @@ public class TestListener implements ITestListener {
     }
 
 
-
-    @Override
     public void onTestSkipped(ITestResult result) {
         System.out.println(String.format("Test '%s' skipped", result.getName()));
 
     }
 
-    @Override
+
     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 
     }
 
-    @Override
+
     public void onStart(ITestContext context) {
 
     }
 
-    @Override
+
     public void onFinish(ITestContext context) {
         context.getPassedTests();
         context.getFailedTests();
